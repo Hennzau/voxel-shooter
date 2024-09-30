0b57b5be0c48a53541e1dca4aa8ef3f09ea888d8
Enzo Le Van <dev@enzo-le-van.fr>
2024-09-30 21:56:20 +0200

    chunk culler done

--- src/logic/world.rs
+++ src/logic/world.rs
@@ -32,6 +32,10 @@ pub struct ChunkData {
     pub z: i32,
 
     pub blocks: [i32; 16 * 16 * 16],
+
+    pub encode_x: [u16; 16 * 16], // y + z * 16
+    pub encode_y: [u16; 16 * 16], // x + z * 16
+    pub encode_z: [u16; 16 * 16], // x + y * 16
 }
 
 #[derive(Debug, Component)]
@@ -59,14 +63,26 @@ fn load_chunks(mut commands: Commands, mut worlds: Query<(Entity, &mut WorldData
 
             let mut blocks = [0; 16 * 16 * 16];
 
+            let mut encode_x = [0; 16 * 16];
+            let mut encode_y = [0; 16 * 16];
+            let mut encode_z = [0; 16 * 16];
+
             for i in 0..16 {
+                // x
                 for j in 0..16 {
+                    // z
                     let height =
                         perlin.get_noise((x * 16 + i) as f64, (z * 16 + j) as f64) as i32 + 16;
 
                     for k in 0..16 {
+                        // y
                         if k < height {
                             blocks[i as usize + k as usize * 16 + j as usize * 16 * 16] = 1;
+                            // for block positionned at (i, k, j), encode the x, y, z: set the right bit to 1 if the block is solid, but "0" is the most significant bit
+
+                            encode_x[k as usize + j as usize * 16] |= 1 << i;
+                            encode_y[i as usize + j as usize * 16] |= 1 << k;
+                            encode_z[i as usize + k as usize * 16] |= 1 << j;
                         }
                     }
                 }
@@ -74,7 +90,15 @@ fn load_chunks(mut commands: Commands, mut worlds: Query<(Entity, &mut WorldData
 
             commands.entity(entity).with_children(|parent| {
                 let id = parent
-                    .spawn(ChunkData { x, y, z, blocks })
+                    .spawn(ChunkData {
+                        x,
+                        y,
+                        z,
+                        blocks,
+                        encode_x,
+                        encode_y,
+                        encode_z,
+                    })
                     .insert(ChunkUpdated)
                     .insert(Name::new(format!("Chunk ({}, {}, {})", x, y, z)))
                     .id();
--- src/render/world/chunk_culler.rs
+++ src/render/world/chunk_culler.rs
@@ -11,79 +11,166 @@ impl ChunkMesh {
         let mut vertices = Vec::<u32>::new();
         let mut indices = Vec::<u32>::new();
 
-        for x in 0..16 {
-            for y in 0..16 {
-                for z in 0..16 {
-                    if data.blocks[x + y * 16 + z * 16 * 16] == 0 {
-                        continue;
-                    }
+        let encode_x = &data.encode_x;
+        let encode_y = &data.encode_y;
+        let encode_z = &data.encode_z;
 
-                    let (x, y, z) = (x as i32, y as i32, z as i32);
+        // Now we are going to make some bitshifts and bitwise operations to know which faces are visible
 
-                    let mut forward = Quad::from_direction(
-                        super::quad::Direction::Forward,
-                        vertices.len(),
-                        bevy::math::IVec3::new(x, y, z),
-                        0,
-                        15,
-                    );
+        for i in 0..16 {
+            for j in 0..16 {
+                let encoded_x = encode_x[i + j * 16];
 
-                    vertices.append(&mut forward.vertices);
-                    indices.append(&mut forward.indices);
+                let visible_left = !(encoded_x << 1) & encoded_x;
+                let visible_right = !(encoded_x >> 1) & encoded_x;
 
-                    let mut back = Quad::from_direction(
-                        super::quad::Direction::Back,
-                        vertices.len(),
-                        bevy::math::IVec3::new(x, y, z),
-                        0,
-                        15,
-                    );
+                // get position of the bits that are set to 1, from
+                let visible_left: Vec<u8> = (0..16)
+                    .filter_map(|i| {
+                        if (visible_left & (1 << i)) != 0 {
+                            Some(i)
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
 
-                    vertices.append(&mut back.vertices);
-                    indices.append(&mut back.indices);
+                let visible_right: Vec<u8> = (0..16)
+                    .filter_map(|i| {
+                        if (visible_right & (1 << i)) != 0 {
+                            Some(i)
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
 
-                    let mut left = Quad::from_direction(
+                for k in visible_left {
+                    let mut quad = Quad::from_direction(
                         super::quad::Direction::Left,
                         vertices.len(),
-                        bevy::math::IVec3::new(x, y, z),
+                        bevy::math::IVec3::new(k as i32, i as i32, j as i32),
                         0,
                         15,
                     );
-                    vertices.append(&mut left.vertices);
-                    indices.append(&mut left.indices);
 
-                    let mut right = Quad::from_direction(
+                    vertices.append(&mut quad.vertices);
+                    indices.append(&mut quad.indices);
+                }
+
+                for k in visible_right {
+                    let mut quad = Quad::from_direction(
                         super::quad::Direction::Right,
                         vertices.len(),
-                        bevy::math::IVec3::new(x, y, z),
+                        bevy::math::IVec3::new(k as i32, i as i32, j as i32),
                         0,
                         15,
                     );
 
-                    vertices.append(&mut right.vertices);
-                    indices.append(&mut right.indices);
+                    vertices.append(&mut quad.vertices);
+                    indices.append(&mut quad.indices);
+                }
 
-                    let mut top = Quad::from_direction(
-                        super::quad::Direction::Up,
-                        vertices.len(),
-                        bevy::math::IVec3::new(x, y, z),
-                        0,
-                        15,
-                    );
+                let encoded_y = encode_y[i + j * 16];
 
-                    vertices.append(&mut top.vertices);
-                    indices.append(&mut top.indices);
+                let visible_bottom = !(encoded_y << 1) & encoded_y;
+                let visible_top = !(encoded_y >> 1) & encoded_y;
 
-                    let mut bottom = Quad::from_direction(
+                let visible_bottom: Vec<u8> = (0..16)
+                    .filter_map(|i| {
+                        if (visible_bottom & (1 << i)) != 0 {
+                            Some(i)
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
+
+                let visible_top: Vec<u8> = (0..16)
+                    .filter_map(|i| {
+                        if (visible_top & (1 << i)) != 0 {
+                            Some(i)
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
+
+                for k in visible_bottom {
+                    let mut quad = Quad::from_direction(
                         super::quad::Direction::Down,
                         vertices.len(),
-                        bevy::math::IVec3::new(x, y, z),
+                        bevy::math::IVec3::new(i as i32, k as i32, j as i32),
                         0,
                         15,
                     );
 
-                    vertices.append(&mut bottom.vertices);
-                    indices.append(&mut bottom.indices);
+                    vertices.append(&mut quad.vertices);
+                    indices.append(&mut quad.indices);
+                }
+
+                for k in visible_top {
+                    let mut quad = Quad::from_direction(
+                        super::quad::Direction::Up,
+                        vertices.len(),
+                        bevy::math::IVec3::new(i as i32, k as i32, j as i32),
+                        0,
+                        15,
+                    );
+
+                    vertices.append(&mut quad.vertices);
+                    indices.append(&mut quad.indices);
+                }
+
+                let encoded_z = encode_z[i + j * 16];
+
+                let visible_back = !(encoded_z << 1) & encoded_z;
+                let visible_forward = !(encoded_z >> 1) & encoded_z;
+
+                let visible_back: Vec<u8> = (0..16)
+                    .filter_map(|i| {
+                        if (visible_back & (1 << i)) != 0 {
+                            Some(i)
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
+
+                let visible_forward: Vec<u8> = (0..16)
+                    .filter_map(|i| {
+                        if (visible_forward & (1 << i)) != 0 {
+                            Some(i)
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
+
+                for k in visible_back {
+                    let mut quad = Quad::from_direction(
+                        super::quad::Direction::Back,
+                        vertices.len(),
+                        bevy::math::IVec3::new(i as i32, j as i32, k as i32),
+                        0,
+                        15,
+                    );
+
+                    vertices.append(&mut quad.vertices);
+                    indices.append(&mut quad.indices);
+                }
+
+                for k in visible_forward {
+                    let mut quad = Quad::from_direction(
+                        super::quad::Direction::Forward,
+                        vertices.len(),
+                        bevy::math::IVec3::new(i as i32, j as i32, k as i32),
+                        0,
+                        15,
+                    );
+
+                    vertices.append(&mut quad.vertices);
+                    indices.append(&mut quad.indices);
                 }
             }
         }
